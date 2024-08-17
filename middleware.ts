b301ed46ccc0fc5601ac328cedb8791a6dbde2d1
Wasif0787 <wasifhussain787@gmail.com>
2024-08-17 16:02:07 +0530

    feat(auth): add Clerk Sign-up and Sign-in

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,18 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 // Define public routes that don't require authentication
 const isPublicRoute = createRouteMatcher([
-    "signin",
-    "signup",
+    "/sign-in",
+    "/sign-up",
     "/",
-    "home"
+    "/home"
 ])
 
 // Define public API routes that don't require authentication
 const isPublicApiRoute = createRouteMatcher([
     "/api/videos"
 ])
-
 /**
  * Clerk Middleware to handle route access based on authentication status.
  * @param {Function} auth - Function to check the authentication status using Clerk.
@@ -22,6 +21,7 @@ const isPublicApiRoute = createRouteMatcher([
 export default clerkMiddleware((auth, req) => {
     const { userId } = auth(); // Extract the userId from the authentication object
     const currentUrl = new URL(req.url); // Extract the current URL from the request
+
     const isAccessingDashboard = currentUrl.pathname === "/home"; // Check if the user is trying to access the dashboard
     const isApiRequest = currentUrl.pathname.startsWith("/api"); // Check if the request is for an API route
 
@@ -34,11 +34,11 @@ export default clerkMiddleware((auth, req) => {
     if (!userId) {
         // Redirect to the sign-in page if trying to access a protected route or a non-public API route
         if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
-            return NextResponse.redirect(new URL("/signin", req.url));
+            return NextResponse.redirect(new URL("/sign-in", req.url));
         }
         // If the request is for a protected API route, redirect to the sign-in page
         if (isApiRequest && !isPublicApiRoute(req)) {
-            return NextResponse.redirect(new URL("/signin", req.url));
+            return NextResponse.redirect(new URL("/sign-in", req.url));
         }
     }
 
@@ -49,4 +49,4 @@ export default clerkMiddleware((auth, req) => {
 // Configuration for matching routes that should apply this middleware
 export const config = {
     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-}
+};
